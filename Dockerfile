eb269a82b958ed1e1a5c4079855f65b26122d571
Khoa TM <tranminhkhoa1402@gmail.com>
2025-04-22 16:55:01 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,14 @@ run curl -fsSL https://deb.nodesource.com/setup_23.x | bash - \
     && apt-get update \
     && apt-get install -y nodejs \
     && apt-get clean \
-    && rm -rf /var/lib/apt/lists/* \
-    && npm install -g @openai/codex
+    && rm -rf /var/lib/apt/lists/*
+
+arg CACHEBUST=202504220452
+run git clone --depth 1 https://github.com/TrustlessComputer/openai-codex.git anon-codex && \
+    cd anon-codex && git pull
+
+run cd anon-codex/codex-cli && npm install && npm run build
 
 run apt-get update && apt-get install sudo -y 
+
+env FORWARD_ALL_MESSAGES=1
--- app/codex_wrapper.py
+++ app/codex_wrapper.py
@@ -17,9 +17,12 @@ async def read_stream(stream: asyncio.StreamReader) -> AsyncGenerator[str, None]
         yield line.decode(errors='ignore')
 
 async def run_codex(prompt: str) -> AsyncGenerator[BaseResponse, None]:
-
+    providerFromEnv = os.getenv("CODEX_PROVIDER", "local")
+    
     command = [
-        "codex",
+        "/workspace/anon-codex/codex-cli/dist/cli.js",
+        "-p",
+        providerFromEnv,
         "--auto-edit",
         "--full-auto",
         "--no-project-doc",
@@ -33,10 +36,11 @@ async def run_codex(prompt: str) -> AsyncGenerator[BaseResponse, None]:
         command_str,
         stdout=asyncio.subprocess.PIPE,
         stderr=sys.stderr,
+        stdin=asyncio.subprocess.PIPE,
         env=os.environ
     )
 
     async for line in read_stream(process.stdout):
         line_json: dict = json.loads(line)
-        print(line_json)
+        print(line_json, file=sys.stderr, flush=True)
         yield create_model_from_dict(line_json)
