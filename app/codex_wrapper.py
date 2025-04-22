bf5a8434b2253d75531327b72038bb38252ef3d6
a87a5d52ca69bd68 <8e2ec6d698e7d4c0>
2025-04-22 10:29:22 +0000

    update

--- app/codex_wrapper.py
+++ app/codex_wrapper.py
@@ -37,6 +37,11 @@ async def run_codex(prompt: str) -> AsyncGenerator[BaseResponse, None]:
     )
 
     async for line in read_stream(process.stdout):
-        line_json: dict = json.loads(line)
-        print(line_json)
+
+        try:
+            line_json: dict = json.loads(line)
+        except json.JSONDecodeError:
+            print(f"Error decoding line: {line} (skipping)")
+            continue
+
         yield create_model_from_dict(line_json)
